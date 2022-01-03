f2bf957a5070987ea1ada60897582fadfc9e2fd6
blacktokki <39031723+blacktokki@users.noreply.github.com>
2022-01-03 18:44:13 +0900

    astar5-1

--- src/UnitList.tsx
+++ src/UnitList.tsx
@@ -8,48 +8,57 @@ import Animated, { useAnimatedStyle, useDerivedValue, useSharedValue, withTiming
 const DURATION = TILESIZE/SPEED_PER_STEP * MS_PER_STEP
 
 function useMoveable(unit:UnitProps){
+    const prevPos = useSharedValue(unit.initPos)
     const targetPos = useSharedValue(unit.initPos)
     const tx = useSharedValue(unit.initPos[0])
     const ty = useSharedValue(unit.initPos[1])
+    const tds = useSharedValue(0)
     const subtargetPos = useDerivedValue(()=>{return [tx.value, ty.value] as Position})
-    const ttx = useDerivedValue(()=>withTiming(tx.value, {duration:DURATION, easing:Easing.linear}, animCallback))
-    const tty = useDerivedValue(()=>withTiming(ty.value, {duration:DURATION, easing:Easing.linear}, animCallback))
-    const callback = ()=>{
-        if(subtargetPos.value[0]!=targetPos.value[0] || subtargetPos.value[1] != targetPos.value[1]){
-            const _pos = subtargetPos.value.map((value, index)=>{
-                const res = value - value % TILESIZE
-                if (value < targetPos.value[index])
-                    return res + TILESIZE
-                if (value > targetPos.value[index])
-                    return res - TILESIZE
-                return res
-            }) as Position
-            tx.value = _pos[0]
-            ty.value = _pos[1]
+    const runs = useDerivedValue(()=>{
+        if (tds.value == 0){
+            return withTiming(0, {duration:1}, (fin)=>{
+                if(fin)callback()
+            })
         }
         else
+            return withTiming(1, {duration:DURATION/TILESIZE * tds.value, easing:Easing.linear}, (fin)=>{
+                if(fin){
+                    prevPos.value = subtargetPos.value
+                    tds.value = 0
+                }
+            })
+    })
+    const ttx = useDerivedValue(()=>prevPos.value[0] * (1 - runs.value) + subtargetPos.value[0] * runs.value)
+    const tty = useDerivedValue(()=>prevPos.value[1] * (1 - runs.value) + subtargetPos.value[1] * runs.value)
+    const callback = ()=>{
+        if(subtargetPos.value[0]!=targetPos.value[0] || subtargetPos.value[1] != targetPos.value[1]){
+            // const _pos = subtargetPos.value.map((value, index)=>{
+            //     const res = value - value % TILESIZE
+            //     if (value < targetPos.value[index])
+            //         return res + TILESIZE
+            //     if (value > targetPos.value[index])
+            //         return res - TILESIZE
+            //     return res
+            // }) as Position
+            // tx.value = _pos[0]
+            // ty.value = _pos[1]
+            tds.value = Math.sqrt(Math.pow(targetPos.value[0] - tx.value, 2) + Math.pow(targetPos.value[1] - ty.value, 2))
+            tx.value = targetPos.value[0]
+            ty.value = targetPos.value[1]
+        }
+        else{
             unit.moveFinished && unit.moveFinished(unit)
+        }
     }
     unit.setTargetPos = (pos) =>{
         targetPos.value = pos;
         callback()
     }
-    let finished = 2
-    let isFirst = true
-    const animCallback = (isFinite?:boolean) =>{
-        finished += 1
-        if (isFinite && finished==2){
-            if(isFirst){
-                setTimeout(callback, Math.random() * DURATION)
-                isFirst = false
-            }
-            else
-                callback()
-        }
-    }
+    useEffect(()=>{
+        setTimeout(callback, Math.random() * 128)
+    }, [])
 
     const animStyle = useAnimatedStyle(() => {
-        finished = 0
         const style = {
             ...StyleSheet.absoluteFillObject,
             transform: [
@@ -67,6 +76,14 @@ function useMoveable(unit:UnitProps){
         },
         [ttx, tty]
     );
+    unit.id ==1 && useAnimatedReaction(
+        () => {
+            console.log(tds.value)
+        }, 
+        (result, previous) => {
+        },
+        [tds]
+    )
     return [ttx, tty, targetPos, animStyle] as [Animated.SharedValue<number>, Animated.SharedValue<number> ,  Animated.SharedValue<Position>, typeof animStyle]
 }
 
